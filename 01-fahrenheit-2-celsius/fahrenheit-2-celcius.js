44d9da00df07c4484af213d3145655c2b6a874c0
Steffen Korth <steffen.korth@de.ibm.com>
2020-01-29 11:30:49 +0100

    Lab completed

---
+++ 01-fahrenheit-2-celsius/fahrenheit-2-celcius.js
@@ -0,0 +1,6 @@
+let fahrenheit2celcius = (fahrenheit) => {
+    celcius = (fahrenheit - 32) * 5 / 9;
+    return celcius;
+}
+
+module.exports = { fahrenheit2celcius};
---
+++ 01-fahrenheit-2-celsius/fahrenheit-2-celcius.spec.js
@@ -0,0 +1,32 @@
+const { fahrenheit2celcius } = require('./fahrenheit-2-celcius.js');
+
+describe('the fahrenheit-2-celcius canary spec', () => {
+  it('shows the infrastructure works', () => {
+    expect(true).toBe(true);
+  });
+
+  describe('the fahrenheit-2-celcius should behave as follows', () => {
+
+    it('function fahrenheit2celcius exists', () => {
+      expect(fahrenheit2celcius).toBeDefined();
+    });
+
+    it('converts 50 F to 10C', () => {
+      expect(fahrenheit2celcius(50)).toBe(10);
+    });
+
+    it('convert 212 F to 100 C', () => {
+      expect(fahrenheit2celcius(212)).toBe(100);
+    });
+
+    it('convert 32 F to 0 C', () => {
+      expect(fahrenheit2celcius(32)).toBe(0);
+    });
+
+    it('convert -40 F to -40 C', () => {
+      expect(fahrenheit2celcius(-40)).toBe(-40);
+    });
+
+  });
+
+});
