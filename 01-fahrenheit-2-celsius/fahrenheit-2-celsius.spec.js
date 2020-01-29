cf98ff5ea308b8e6b1484014ee61e2d014bbd922
Steffen Korth <steffen.korth@de.ibm.com>
2020-01-29 10:35:04 +0100

    initial commit

---
+++ 01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -0,0 +1,26 @@
+describe('the fahrenheit-2-celcius canary spec', () => {
+  it('shows the infrastructure works', () => {
+    expect(true).toBe(true);
+  });
+
+  describe('the fahrenheit-2-celcius should behave as follows', () => {
+
+    it('function fahrenheit2celcius exists', () => {
+      expect(fahrenheit2celcius).toBeDefined();
+    });
+
+    xit('converts 50 F to 10C', () => {
+    });
+
+    xit('convert 212 F to 100 C', () => {
+    });
+
+    xit('convert 32 F to 0 C', () => {
+    });
+
+    xit('convert -40 F to -40 C', () => {
+    });
+
+  });
+
+});
