09f88a6f4681fc0410eb5139c551de979c758847
Steffen Korth <steffen.korth@de.ibm.com>
2020-01-29 11:49:26 +0100

    second test

--- 01-fahrenheit-2-celsius/fahrenheit-2-celcius.spec.js
+++ 01-fahrenheit-2-celsius/fahrenheit-2-celcius.spec.js
@@ -1,14 +1,19 @@
-const { fahrenheit2celcius } = require('./fahrenheit-2-celcius.js');
+const { fahrenheit2celcius, celcius2fahrenheit } = require('./fahrenheit-2-celcius.js');
 
-describe('the fahrenheit-2-celcius canary spec', () => {
+
+describe('the fahrenheit-celcius canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
 
-  describe('the fahrenheit-2-celcius should behave as follows', () => {
+  describe('the fahrenheit-celcius conversion should behave as follows', () => {
 
     it('function fahrenheit2celcius exists', () => {
       expect(fahrenheit2celcius).toBeDefined();
+    });
+
+    it('function celcius2fahrenheit exists', () => {
+      expect(celcius2fahrenheit).toBeDefined();
     });
 
     it('converts 50 F to 10C', () => {
@@ -25,6 +30,22 @@ describe('the fahrenheit-2-celcius canary spec', () => {
 
     it('convert -40 F to -40 C', () => {
       expect(fahrenheit2celcius(-40)).toBe(-40);
+    });
+/* second section */
+    it('converts 10 C to 50 C', () => {
+      expect(elcius2fahrenheit(10)).toBe(50);
+    });
+
+    it('convert 212 F to 100 C', () => {
+      expect(elcius2fahrenheit(100)).toBe(212);
+    });
+
+    it('convert 0 C to 32 C', () => {
+      expect(celcius2fahrenheit(0)).toBe(32);
+    });
+
+    it('convert -40 C to -40 F', () => {
+      expect(celcius2fahrenheit(-40)).toBe(-40);
     });
 
   });
